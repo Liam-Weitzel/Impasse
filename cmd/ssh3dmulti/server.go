7eec554a33b29ec81185225c642e8624d13cb534
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2022-05-12 02:42:29 +0200

    started with multiuser system

---
+++ cmd/ssh3dmulti/server.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"strings"
+)
+
+type server struct {
+	connection string
+	cmds       chan func(*server)
+	cons       map[net.Conn]chan string
+	quit       bool
+}
+
+func newServer(connection string) *server {
+	return &server{
+		connection: connection,
+		cmds:       make(chan func(*server)),
+		cons:       make(map[net.Conn]chan string),
+	}
+}
+
+func (s *server) run(done chan struct{}) error {
+
+	var network, addr string
+	idx := strings.IndexRune(s.connection, ':')
+	if idx < 0 {
+		network = "tcp"
+		addr = s.connection
+	} else {
+		network = s.connection[:idx]
+		addr = s.connection[idx+1:]
+	}
+
+	listener, err := net.Listen(network, addr)
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	go func() {
+		for !s.quit {
+			con, err := listener.Accept()
+			if err != nil {
+				s.cmds <- func(s *server) {
+					log.Printf("listen failed: %v\n", err)
+					s.quit = true
+				}
+				return
+			}
+			log.Println("accepted")
+			s.cmds <- func(s *server) {
+				log.Println("new connection")
+
+				out := make(chan string, 5)
+
+				go func() {
+					for msg := range out {
+						if _, err := con.Write([]byte(msg)); err != nil {
+							s.cmds <- func(s *server) {
+								con.Close()
+								delete(s.cons, con)
+							}
+						}
+					}
+				}()
+
+				s.cons[con] = out
+				go func() {
+					sc := bufio.NewScanner(con)
+					for sc.Scan() {
+						msg := sc.Text()
+						s.cmds <- func(s *server) {
+							for c, o := range s.cons {
+								if c != con {
+									o <- msg
+								}
+							}
+						}
+					}
+					if err := sc.Err(); err != nil {
+						log.Printf("error: %v\n", err)
+					}
+					s.cmds <- func(s *server) {
+						con.Close()
+						delete(s.cons, con)
+					}
+				}()
+			}
+		}
+	}()
+
+	for !s.quit {
+		select {
+		case <-done:
+			return nil
+		case fn := <-s.cmds:
+			fn(s)
+		}
+	}
+
+	return nil
+}
